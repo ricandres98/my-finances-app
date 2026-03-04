be92eef6614866d84727fc51793f8d415ce9ee4d
Ricardo <ricardo270298@gmail.com>
2026-03-04 17:39:22 -0400

    feat: add logout feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { logout } from "./login/logout";
 
 export default function RootLayout({
   children,
@@ -7,7 +8,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={``}>{children}</body>
+      <body className={``}>
+      <button onClick={logout}>Logout</button>
+        {children}
+      </body>
     </html>
   );
 }
